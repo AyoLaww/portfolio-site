94c12be6f05938adb8abfa7ca801885fe69b4ef7
AyoLaww <104886347+AyoLaww@users.noreply.github.com>
2025-10-31 18:00:29 +0300

    Add Radix UI Accordion and Badge components; implement Matrix animation and ShimmeringText effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
-import { Badge } from "@/components/Badge"
+import { Badge } from "./ui/badge"
+import {Badge as BadgeComponent} from "@/components/Badge"
 import Link from "next/link"
 import { Github, Linkedin, Twitter } from "lucide-react"
+import MatrixComponent from "./Matrix"
+import { ShimmeringText } from "./ui/shimmering-text"
 
 export function Hero() {
   return (
@@ -10,8 +13,10 @@ export function Hero() {
         <div className="text-[10px] tracking-[0.35em] uppercase text-muted-foreground/80 font-figtree">
           hey, my name is
         </div>
-        <h1 className="font-doto text-6xl md:text-7xl lg:text-8xl leading-none">LAWRENCE</h1>
-        <Badge label="Software Engineer" />
+        {/* <h1 className="font-doto text-6xl md:text-7xl lg:text-8xl leading-none">LAWRENCE</h1> */}
+        <MatrixComponent />
+        <br />
+        <Badge variant="outline"><ShimmeringText text="Software Engineer" /></Badge>
 
         <p className="max-w-[680px] text-sm md:text-base text-muted-foreground font-figtree leading-6">
           I thrive on <span className="text-foreground font-semibold">building digital products</span> that don’t
@@ -20,13 +25,13 @@ export function Hero() {
         </p>
         <div className="flex flex-wrap gap-2 pt-1">
           <Link href="https://www.linkedin.com/in/lawrence-simiyu-737b53284/" target="_blank" rel="noopener noreferrer">
-            <Badge icon={<Linkedin className="opacity-80" />} label="LinkedIn" />
+            <BadgeComponent icon={<Linkedin className="opacity-80" />} label="LinkedIn" />
           </Link>
           <Link href="https://x.com/AyoLaww" target="_blank" rel="noopener noreferrer">
-            <Badge icon={<Twitter className="opacity-80" />} label="X" />
+            <BadgeComponent icon={<Twitter className="opacity-80" />} label="X" />
           </Link>
           <Link href="https://github.com/AyoLaww/" target="_blank" rel="noopener noreferrer">
-            <Badge icon={<Github className="opacity-80" />} label="GitHub" />
+            <BadgeComponent icon={<Github className="opacity-80" />} label="GitHub" />
           </Link>
         </div>
       </div>
